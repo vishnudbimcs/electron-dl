b74ff7dd2d502f8eb5874c741dfe4368092f212c
Vishnu Sharma <vishnudbimcs@gmail.com>
2020-06-18 01:00:52 +0530

    allow overwrite
    allow overwriting of exiting file in the same folder with the same name

--- index.js
+++ index.js
@@ -46,7 +46,11 @@ function registerListener(session, options, callback = () => {}) {
 		} else {
 			const filename = item.getFilename();
 			const name = path.extname(filename) ? filename : getFilenameFromMime(filename, item.getMimeType());
-			filePath = unusedFilename.sync(path.join(directory, name));
+			if (options.allowOverwrite) {
+				filePath = path.join(dir, name);
+			} else {
+				filePath = unusedFilename.sync(path.join(dir, name));
+			}
 		}
 
 		const errorMessage = options.errorMessage || 'The download of {filename} was interrupted';
